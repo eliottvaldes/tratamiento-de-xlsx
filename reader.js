71f13e38475f2b8bbb6f0e64e1fb5ab276834b16
JoseMonroy <mnry7999@gmail.com>
2022-09-23 13:13:21 -0500

    Refactorización

--- reader.js
+++ reader.js
@@ -1,10 +1,12 @@
 const XLSX = require('xlsx');
 
-function leerExcel(path) {
-    const workbook = XLSX.readFile(path);
-    const sheet_name_list = workbook.SheetNames;
-    const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-    return xlData;
+class reader {
+    static readXlsx(path) {
+        const workbook = XLSX.readFile(path);
+        const sheet_name_list = workbook.SheetNames;
+        const xlData = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
+        return xlData;
+    }
 }
 
-module.exports = leerExcel;
+module.exports = reader;
