1e1f774eec4b66bf1627ebabd2e9e3d25407dd61
JoseMonroy <mnry7999@gmail.com>
2022-10-17 20:17:12 -0500

    Automatización para nombre de archivo vcf

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fileController = require('./lib/controller/fileController');
-
+const excelController = require('./lib/controller/excelController');
 
 fileController.createFile();
 fileController.addInfo('./public/file.xlsx');
-fileController.moveFile();
+const fileName = excelController.getFileName('./public/file.xlsx');
+fileController.moveFile(fileName);
