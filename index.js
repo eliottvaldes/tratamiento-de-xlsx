ffbe9243ac712e8b9cd9942d87a74bc95560d463
=eliot valdes <=eliottvaldes@hotmail.com>
2022-10-17 23:44:19 -0500

    upd -> define more variables to make easier the treatement

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const fileController = require('./lib/controller/fileController');
 const excelController = require('./lib/controller/excelController');
 
+// this variable contains the name of the file that will be treated and created
+const fileNameToTreat = '18-10-22';
+
+// define the full path of the file to treat
+const file = `./public/${fileNameToTreat}.xlsx`;
+
+
 fileController.createFile();
-fileController.addInfo('./public/file.xlsx');
-const fileName = excelController.getFileName('./public/file.xlsx');
+fileController.addInfo(file);
+const fileName = excelController.getFileName(file);
 fileController.moveFile(fileName);
