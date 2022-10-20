8ba1d4cc144a00e47c8f42547acc07c33adbfb44
=eliot valdes <=eliottvaldes@hotmail.com>
2022-10-19 23:27:12 -0500

    upd -> change fileName

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fileController = require('./lib/controller/fileController');
 const excelController = require('./lib/controller/excelController');
 
 // this variable contains the name of the file that will be treated and created
-const fileNameToTreat = '18-10-22';
+const fileNameToTreat = '19-10-22-david';
 
 // define the full path of the file to treat
 const file = `./public/${fileNameToTreat}.xlsx`;
